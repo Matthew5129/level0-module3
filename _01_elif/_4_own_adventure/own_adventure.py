3a742644dbb3797f814494d998c8aae997e9c09b
league <jack.stewart.hay@gmail.com>
2024-07-12 16:54:39 -0700

    Matthew5129 left the game

--- _01_elif/_4_own_adventure/own_adventure.py
+++ _01_elif/_4_own_adventure/own_adventure.py
@@ -12,3 +12,39 @@ if user == "Ok":
     messagebox.showinfo(title="user", message="Option has been added to your story!")
 if user == "No":
     simpledialog.askstring(title="user", prompt="Second option. One time there lived a smart boy named Bob. He had 1000 IQ and could create Minecraft traps that no one could escape.")
+if user == "Ok":
+    messagebox.showinfo(title="user", message="Option has been added to your story!")
+if user == "No":
+    simpledialog.askstring(title="user", prompt="Third option. Since Bob was so stupid, everyone told him to go back to preschool. Bob was sad, and for some reason, he told his parents to make him go back to preschool.")
+if user == "Ok":
+    messagebox.showinfo(title="user", message="Option has been added to your story!")
+if user == "No":
+    simpledialog.askstring(title="user", prompt="Fourth option. Since Bob was so good at Minecraft, every single one of his best friends left, leaving him all alone. That's too bad.")
+if user == "Ok":
+    messagebox.showinfo(title="user", message="Option has been added to your story!")
+if user == "No":
+    simpledialog.askstring(title="user", prompt="Fifth option. His parents asked why; he was in 5th grade! He was nervous to admit his stupidity, but he had no choice but to tell them he had 0 IQ.")
+if user == "Yes":
+    messagebox.showinfo(title="user", message="Option has been added to your story!")
+if user == "No":
+    simpledialog.askstring(title="user", prompt="Sixth option. For 3 hours straight, Bob played his boring Minecraft survival server. No join messages were sent. But just before he left the game, someone he never heard of joined.")
+if user == "Yes":
+    messagebox.showinfo(title="user", message="Option has been added to your story!")
+if user == "No":
+    simpledialog.askstring(title="user", prompt="Seventh option. Suddenly, a shadow appeared and told Bob he would make him 100 IQ for the rest of eternity. Bob was happy. Now it was payback time.")
+if user == "Yes":
+    messagebox.showinfo(title="user", message="Option has been added to your story!")
+if user == "No":
+    simpledialog.askstring(title="user", prompt="Eighth option. Bob asked who the new entity was. It's nametag read: Herobrine. What in the world is a Herobrine? Bob was confused.")
+if user == "Yes":
+    messagebox.showinfo(title="user", message="Option has been added to your story!")
+if user == "No":
+    simpledialog.askstring(title="user", prompt="Ninth option. Bob quit preschool and went back to 5th grade. 'Hey guys! Guess what? I'm back! And I'm smarter than ever!' ")
+if user == "Yes":
+    messagebox.showinfo(title="user", message="Option has been added to your story!")
+if user == "No":
+    simpledialog.askstring(title="user", prompt="Tenth option. Suddenly, the entity chased after Bob. Bob started attacking it with his fully enchanted netherite sword. It did barely any damage. After twenty hits, he thought it was almost dead. Suddenly, Herobrine shot purple fireballs at Bob.he rar as far away as possible until he reached a dead end. Herobrine Shot a huge fireball and Bob instantly died.")
+if user == "Yes":
+    messagebox.showinfo(title="user", message="Option has been added to your story!")
+if user == "No":
+    simpledialog.askstring(title="user", prompt="Eleventh option. 'Well dumb-dumb, let's see how much your IQ has improved!' Bob did a evil smile. 'What is 7 factorial, dummies?")
