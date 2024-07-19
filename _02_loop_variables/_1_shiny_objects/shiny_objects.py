0c3bef1923afe84da6df74d3807b38a9c27a1e47
league <rutdyytduydyt>
2024-07-19 16:59:19 -0700

    Matthew5129 left the game

--- _02_loop_variables/_1_shiny_objects/shiny_objects.py
+++ _02_loop_variables/_1_shiny_objects/shiny_objects.py
@@ -15,7 +15,7 @@ def many_shiny_objects():
     # TODO 1) Call the method above to play Mister Zee
 
     # TODO 2) Ask the user how many shiny objects they want
-
+    simpledialog.askstring(title="user", prompt="How many shiny objects do you want?")
     # TODO 3) Play the sound that many times
 
     pass
@@ -25,3 +25,75 @@ if __name__ == '__main__':
     window = Tk()
     window.withdraw()
     many_shiny_objects()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- _02_loop_variables/_2_turtle_walk_of_fame/walk_of_fame.py
+++ _02_loop_variables/_2_turtle_walk_of_fame/walk_of_fame.py
@@ -1,16 +1,52 @@
 import turtle
 
 if __name__ == '__main__':
-    my_turtle = turtle.Turtle()
-    my_turtle.shape('turtle')
-    my_turtle.color('blue', 'green')
-    my_turtle.speed(100)
+    bob = turtle.Turtle()
+    bob.shape('turtle')
+    bob.shapesize(2)
+    bob.color('blue', 'green')
+    bob.speed(100)
 
     # TODO 1) Set the X position of the turtle so that it starts on the left.
-
+    bob.penup()
+    bob.setx(-400)
     # TODO 2) Make the turtle draw a star shape. Hint: angle=144.
+    bob.pendown()
 
     # TODO 3) Set the length of each line in the star to 30
+    for j in range(10):
+        for i in range(5):
+            bob.forward(100)
+            bob.right(144)
+        bob.penup()
+        bob.forward(100)
+        bob.pendown()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     # TODO: CHALLENGE
     #       Make the turtle draw a line of stars like the image in
