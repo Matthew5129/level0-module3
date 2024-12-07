3ec5b2f7dcdf0c605fb233425d470d25dcbfeae9
league <bob@>
2024-12-06 16:22:41 -0800

    gyiyfkfuykfukuf

--- _01_elif/_3_are_you_happy/are_you_happy.py
+++ _01_elif/_3_are_you_happy/are_you_happy.py
@@ -5,11 +5,11 @@ if __name__ == '__main__':
     # TODO: Look at the AreYouHappy.png image
     #       Use pop-ups to recreate the chart using if and elif statements
     window = Tk()
-    user = simpledialog.askstring(title="user", prompt="HELLO, MISTER! I AM THE HAPPY SCREAMING MAN! ARE YOU HAPPY?")
+    user = simpledialog.askstring(title="user", prompt="HELLO, MISTER! I AM HAPPY! ARE YOU HAPPY?")
     if user == "Yes":
         messagebox.showinfo(title="user", message="GOOD JOB BEING A HAPPY MAN, MISTER!")
     if user == "No":
-        simpledialog.askstring(title="user", prompt="DO YOU WANT TO BE HAPPY?")
-    if user == "No":
-        messagebox.showinfo(title="user", message="OK...NOW I'M SAD.")
+        simpledialog.askstring(title="user", prompt="DO YOU WANT TO BE HAPPY, MISTER?")
+    if user == "NO! SHUT UP!":
+        messagebox.showinfo(title="user", message="YOU'RE MEAN, MISTER!")
     pass
